3e89e8827a5fb225b722fc2448044946d864979b
Kylin <Kylin@DESKTOP-UCVQMOL>
2019-06-20 21:36:46 +0800

    初次提交版本

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,36 +1,18 @@
 DROP DATABASE IF EXISTS product_master;
 CREATE DATABASE product_master;
-CREATE TABLE product_master.product(
+CREATE TABLE product_master.t_product(
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
   price DOUBLE(10,2) NOT NULL DEFAULT 0
 );
-INSERT INTO product_master.product (name, price) VALUES('master', '1');
+INSERT INTO product_master.t_product (name, price) VALUES('master', '1');
 
 
-DROP DATABASE IF EXISTS product_slave_alpha;
-CREATE DATABASE product_slave_alpha;
-CREATE TABLE product_slave_alpha.product(
+DROP DATABASE IF EXISTS product_slave;
+CREATE DATABASE product_slave;
+CREATE TABLE product_slave.t_product(
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
   price DOUBLE(10,2) NOT NULL DEFAULT 0
 );
-INSERT INTO product_slave_alpha.product (name, price) VALUES('slaveAlpha', '1');
-
-DROP DATABASE IF EXISTS product_slave_beta;
-CREATE DATABASE product_slave_beta;
-CREATE TABLE product_slave_beta.product(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  price DOUBLE(10,2) NOT NULL DEFAULT 0
-);
-INSERT INTO product_slave_beta.product (name, price) VALUES('slaveBeta', '1');
-
-DROP DATABASE IF EXISTS product_slave_gamma;
-CREATE DATABASE product_slave_gamma;
-CREATE TABLE product_slave_gamma.product(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  price DOUBLE(10,2) NOT NULL DEFAULT 0
-);
-INSERT INTO product_slave_gamma.product (name, price) VALUES('slaveGamma', '1');
+INSERT INTO product_slave.t_product (name, price) VALUES('slave', '1');
